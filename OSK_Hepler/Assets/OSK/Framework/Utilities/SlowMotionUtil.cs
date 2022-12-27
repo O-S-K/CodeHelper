f51bc88ef906b9d4ac609f1bbac2a7c1079f329a
Osk <52168556+O-S-K@users.noreply.github.com>
2022-12-27 11:06:25 +0700

    slow motion

---
+++ OSK_Hepler/Assets/OSK/Framework/Utilities/SlowMotionUtil.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OSK
+{
+    public class SlowMotionUtil : MonoBehaviour
+    {
+        public float slowdownFactor = 0.05f;
+        public float slowdownLength = 2f;
+        void Update()
+        {
+            Time.timeScale += (1f / slowdownLength) * Time.unscaledDeltaTime;
+            Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+        }
+
+        public void DoSlowmotion()
+        {
+            Time.timeScale = slowdownFactor;
+            Time.fixedDeltaTime = Time.timeScale * .02f;
+        }
+    }
+}
--- OSK_Hepler/Assets/OSK/Framework/Utilities/TimeUtils.cs
+++ OSK_Hepler/Assets/OSK/Framework/Utilities/TimeUtils.cs
@@ -12,16 +12,24 @@ namespace OSK.Utils
         private static MonoBehaviour behaviour;
         public delegate void Task();
 
-        //TimeUtils.Schedule(this, 0.5f, () => { });
-        public static void Schedule(MonoBehaviour _behaviour, float delay, Task task)
+        public static void Schedule(MonoBehaviour _behaviour, float delay, Task task, bool unscaleTime = false)
         {
             behaviour = _behaviour;
-            behaviour.StartCoroutine(DoTask(task, delay));
+            if (unscaleTime)
+                behaviour.StartCoroutine(DoTaskUnscale(task, delay));
+            else
+                behaviour.StartCoroutine(DoTask(task, delay));
         }
 
         private static IEnumerator DoTask(Task task, float delay)
         {
             yield return new WaitForSeconds(delay);
+            task();
+        }
+
+        private static IEnumerator DoTaskUnscale(Task task, float delay)
+        {
+            yield return new WaitForSecondsRealtime(delay);
             task();
         }
 
